37713bbda72cdb2317076927edb70fd47eb4ea4a
MatasKa <matas.k2002@gmail.com>
2025-04-05 21:27:36 +0200

    added start and quit to main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,16 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public PlayerInput input;
+
+    private void Awake()
     {
-        
+        input = new PlayerInput();
+        input.Enable();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnA()
     {
-        
+        Debug.Log("Jump (A) was pressed!");
+        SceneManager.LoadScene("Arena");
+    }
+    public void OnB()
+    {
+        Debug.Log("B was pressed!");
+        Application.Quit();
     }
 }
--- Assets/VIA Input/Input.cs
+++ Assets/VIA Input/Input.cs
@@ -5,7 +5,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Player : MonoBehaviour
+public class Input : MonoBehaviour
 {
     private Vector2 movement;
     private float speed = 5;
@@ -15,42 +15,42 @@ public class Player : MonoBehaviour
     {
         transform.Translate(movement);
     }
-    
+
     public void OnMovement(InputValue value)
     {
-        movement = value.Get<Vector2>()*Time.deltaTime*speed;
+        movement = value.Get<Vector2>() * Time.deltaTime * speed;
     }
-    
+
     public void OnA()
     {
         text.text = "A";
     }
-    
+
     public void OnB()
     {
         text.text = "B";
     }
-    
+
     public void OnX()
     {
         text.text = "X";
     }
-    
+
     public void OnY()
     {
         text.text = "Y";
     }
-    
+
     public void OnLeftTrigger()
     {
         text.text = "L Trigger";
     }
-    
+
     public void OnRightTrigger()
     {
         text.text = "R Trigger";
     }
-    
+
     public void OnStart()
     {
         text.text = "Start";
