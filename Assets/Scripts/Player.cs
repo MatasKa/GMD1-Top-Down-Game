220f8ae4ee303a88f48dae60b9d880db2b5e91d9
MatasKa <matas.k2002@gmail.com>
2025-04-06 14:30:24 +0200

    added walking and animations
    updated the walking script by moving the Time.fixedDeltaTime to FixedUpdate instead of OnMovement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,14 +8,15 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    private Vector2 movementInput;
     private Vector2 movement;
     private float speed = 3;
     public Animator animator;
-    private Vector2 moveInput;
     private Vector2 lastMoveDir;
 
-    void Update()
+    void FixedUpdate()
     {
+        movement = movementInput * speed * Time.fixedDeltaTime;
         transform.Translate(movement);
 
         if (movement != Vector2.zero)
@@ -26,11 +27,10 @@ public class Player : MonoBehaviour
         animator.SetFloat("MoveX", movement.x);
         animator.SetFloat("MoveY", movement.y);
         animator.SetBool("IsMoving", movement != Vector2.zero);
-
     }
 
     public void OnMovement(InputValue value)
     {
-        movement = value.Get<Vector2>() * Time.deltaTime * speed;
+        movementInput = value.Get<Vector2>();
     }
 }
