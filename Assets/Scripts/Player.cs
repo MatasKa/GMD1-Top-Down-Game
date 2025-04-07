a266ab7d99ab3c4b2af5bcf23d6174d34921d8e5
MatasKa <matas.k2002@gmail.com>
2025-04-07 20:37:57 +0200

    Added colliders and updated movement script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,14 +10,22 @@ public class Player : MonoBehaviour
 {
     private Vector2 movementInput;
     private Vector2 movement;
-    private float speed = 3;
+    private float speed = 10;
     public Animator animator;
     private Vector2 lastMoveDir;
+    private Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
 
     void FixedUpdate()
     {
+
         movement = movementInput * speed * Time.fixedDeltaTime;
-        transform.Translate(movement);
+        //transform.Translate(movement);
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
 
         if (movement != Vector2.zero)
         {
