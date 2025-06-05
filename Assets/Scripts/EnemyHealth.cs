e8a9c798fcbf800e2606a214cf0fa88f8c732f3a
MatasKa <matas.k2002@gmail.com>
2025-06-05 19:40:11 +0200

    Added hit and kill particles

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -2,11 +2,20 @@ using UnityEngine;
 
 public class EnemyHealth : Health
 {
+    public GameObject hitEffect;
+    public GameObject killEffect;
     protected override void Die()
     {
         Enemy enemy = GetComponent<Enemy>();
         GoldManager goldManager = FindFirstObjectByType<GoldManager>();
         goldManager.AddGold(enemy.gold);
+        Instantiate(killEffect, transform.position, Quaternion.identity);
         base.Die();
+    }
+
+    public override void TakeDamage(int damage)
+    {
+        base.TakeDamage(damage);
+        Instantiate(hitEffect, transform.position, Quaternion.identity);
     }
 }
